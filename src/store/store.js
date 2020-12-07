8fee3e8fcd4d2068c556e8447e7e3e43f866efa4
Sevdi Merve Eroğlu <sevdimerve@gmail.com>
2020-12-07 08:36:25 +0300

    update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -9,37 +9,7 @@ const store = new Vuex.Store({
     firstCreate: true,
     detail: {}
   },
-  actions: {
-    login({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        axios({
-          url: "https://api.codementum.co.uk/auth/login",
-          data: user,
-          method: "POST"
-        })
-          .then(resp => {
-            let info = resp.data;
-            let t = info.data;
-            const token = t.auth.accessToken;
-            const type = t.auth.tokenType;
-            const user = resp.data.user;
-            this.state.authorization = type + " " + token;
-            localStorage.setItem("token", this.state.authorization);
-            axios.defaults.headers.common["Authorization"] = token;
-            resolve(resp);
-          })
-          .catch(err => {
-            localStorage.removeItem("token");
-            reject(err);
-          });
-      });
-    }
-  },
-  mutations: {
-    show: state => {
-      state.isShow = !state.isShow;
-    }
-  }
+
 });
 
 export default store;
